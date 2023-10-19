fc243c45148dfa40112280e0ed9c8b5669af4ff9
Bruno Gabriel <brunogabryel2003@gmail.com>
2023-10-18 23:40:17 -0300

    .

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,24 @@
 import 'package:flutter/material.dart';
 
-class MyWidget extends StatelessWidget {
-  const MyWidget({super.key});
+void main() => runApp(const MeuApp());
 
+class MeuApp extends StatelessWidget {
+  const MeuApp({Key? key}) : super(key: key);
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return MaterialApp(
+      home: Column(
+        children: [
+          Scaffold(
+            appBar: AppBar(
+              backgroundColor: Color.fromRGBO(0, 27, 36, 45),
+              title: const Center(
+                child: Text('Mars Rover'),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
